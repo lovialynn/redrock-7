eab3992e7a6ab1947fb06e779c0629098fba1d0b
lovialynn <56706352+lovialynn@users.noreply.github.com>
2019-12-01 18:26:41 +0800

    Update 2.js

--- 2.js
+++ 2.js
@@ -4,7 +4,6 @@ let int = document.getElementsByClassName('inp')[0],
 
 function ajax(options) {
     var xhr = null;
-    var params = options.data;
     if (window.XMLHttpRequest) {
         xhr = new XMLHttpRequest()
     } else {
@@ -14,38 +13,40 @@ function ajax(options) {
         xhr.open(options.type, options.url + options.data, options.async);
         xhr.send()
     } else if (options.type == "POST") {
-        xhr.open(options.type, options.url,options.type);
+        xhr.open(options.type, options.url, options.type);
         xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
         xhr.send(options.data);
     }
-    let head = "<tr>+<th width='5%'>ID</th>+<th width='30%'>node_id</th>+<th width='20%'>name</th></tr>";
-    let sum = ""
+
 
     xhr.onreadystatechange = function () {
 
         if (xhr.readyState === 4) {
             if (xhr.status === 200) {
-                const json = JSON.parse(xhr.responseText)
+                var data = xhr.responseText;
+                options.success(data);
+            } else options.error()
 
-                for (let i = 0; i < json.items.length; i++) {
-                    sum = sum + "<tr>" + "<td width='5%'>" + json.items[i].id + "</td>" + "<td width='30%'>" + json.items[i].node_id + "</td>" + "<td width='20%'>" + json.items[i].name + "</td>" + "</tr>";
-                }
-                table.innerHTML = head + sum
-               
-            }
-            
 
         }
+
+
     }
-   
 }
 but.addEventListener('click', () => ajax({
     url: 'https://api.github.com/search/repositories?q=',
     type: "GET",
     async: true,
     data: int.value,
-    success: function (data) {
-        console.log(data);
+    success: (data) =>  {
+        data = JSON.parse(data)
+        let head = "<tr>+<th width='5%'>ID</th>+<th width='30%'>node_id</th>+<th width='20%'>name</th></tr>";
+        let sum = ""
+        for (let i = 0; i < data.items.length; i++) {
+            sum = sum + "<tr>" + "<td width='5%'>" + data.items[i].id + "</td>" + "<td width='30%'>" + data.items[i].node_id + "</td>" + "<td width='20%'>" + data.items[i].name + "</td>" + "</tr>";;
+        }
+        table.innerHTML = head + sum
+    },
+    error: () => {alert('失败')
     }
-
 }))
